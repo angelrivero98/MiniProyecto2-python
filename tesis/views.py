b021920e9f3319a6fac7584f545b4ea4e49adf74
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-07-11 22:03:34 -0400

    Se envian los roles al front lal pedir los usuarios

--- tesis/views.py
+++ tesis/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from tesis.forms import UsuarioForm
-from tesis.models import Usuario
+from tesis.models import Usuario, Rol
 # Create your views here.
 
 
 def users(request):
     if request.method == "GET":
         usuarios = Usuario.objects.all()
+        roles = Rol.objects.all()
         form = UsuarioForm()
-        return render(request, "usuarios.html", {'usuarios': usuarios, 'form': form})
+        return render(request, "usuarios.html", {'usuarios': usuarios, 'form': form, 'roles': roles})
     elif request.method == "POST":
         form = UsuarioForm(request.POST)
         if form.is_valid():
