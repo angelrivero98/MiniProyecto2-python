db906b84cf0d6e778d306e74c3072e9c23a3860e
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-07-14 17:43:16 -0400

    Nueva forma de hacer las vistas

--- tesis/urls.py
+++ tesis/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.users, name='users'),
+    path('users/', views.UsersView.as_view(), name='users'),
     path('tesis/', views.tesis,name= 'tesis'),
     path('', views.home, name='home')
 ]
--- tesis/views.py
+++ tesis/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 from tesis.models import Usuario, Tesis, Autor, Evaluador
 from tesis.forms import UsuarioForm, FormInicio, AutorForm
 from tesis.filters import SearchFilter
@@ -17,12 +18,14 @@ def home(request):
         return render(request, 'home.html', {'form': form})
 
 
-def users(request):
-    if request.method == "GET":
+class UsersView(View):
+
+    def get(self, request, *args, **kargs):
         usuarios = Usuario.objects.all()
         form = UsuarioForm()
         return render(request, "usuarios.html", {'usuarios': usuarios, 'form': form})
-    elif request.method == "POST":
+
+    def post(self, request, *args, **kargs):
         logging.info('Creando usuario')
         data = request.POST.copy()
         data['date_joined'] = datetime.datetime.now()
@@ -41,7 +44,7 @@ def users(request):
             logging.error(
                 'Error creando el usuario. El formulario no es válido' + str(request.POST))
             return render(request, 'usuarios.html', {'form': form})
-
+                  
 
 def tesis(request):
     if request.method == "GET":
